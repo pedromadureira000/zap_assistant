9c71b0e1965904d23ea6426e38db6c66c2966a80
Pedro Madureira <dev@pedromadureira.xyz>
2023-06-06 16:56:08 -0300

    error feedback to user

--- ai_experiment/core/tasks.py
+++ ai_experiment/core/tasks.py
@@ -5,6 +5,7 @@ import sentry_sdk
 
 from ai_experiment.core.facade import add_completion_to_conversation, get_chat_completion, send_completion_to_user_with_mega_api
 from ai_experiment.core.models import Conversation
+from ai_experiment.mega_api.models import MegaAPIInstance
 from ai_experiment.user.models import UserModel
 
 
@@ -33,7 +34,12 @@ def get_completion_and_send_to_user(self, user_id, user_txt_input, conversation_
                 mega_api_instance_phone,
                 completion
             )
+            return "Done"
     except Exception as exc:
         sentry_sdk.capture_exception(exc)
         raise self.retry(exc=exc, countdown=60, max_retries=2)
-    return "Done"
+    mega_api_instance = MegaAPIInstance.objects.first()
+    if mega_api_instance:
+        #  err_msg = "Sorry, we are having some problems with our servers. Please try again later."
+        err_msg = "Desculpe, estamos tendo alguns problemas com nossos servidores. Por favor, tente novamente mais tarde."  # XXX fix it
+        mega_api_instance.send_text_message(user.whatsapp, err_msg)
